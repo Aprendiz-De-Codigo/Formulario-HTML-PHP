13406318ab450f44717390d0f84116f679e1ee63
Aprendiz-De-Codigo <johnbailarin7@gmail.com>
2024-04-22 17:44:44 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <form action="" method="post" autocomplete="off">
+        <img src="img/logo.svg" alt="">
+        <div class="input-group">
+            <div class="input-container">
+                <input type="text" name="name" placeholder="Nombre" required>
+                <i class="fa-solid fa-user"></i>
+            </div>
+
+            <div class="input-container">
+                <input type="password" name="password" placeholder="Contraseña" required> 
+                <i class="fa-solid fa-lock"></i>
+            </div>
+
+            <div class="input-container">
+                <input type="email" name="email" placeholder="Correo" required>
+                <i class="fa-solid fa-envelope"></i>
+            </div>
+
+            <div class="input-container">
+                <input type="tel" name="phone" placeholder="Teléfono" required>
+                <i class="fa-solid fa-phone"></i>
+            </div>
+
+            <a href="#">Terminos y condiciones</a>
+
+            <input name="send" type="submit" class="btn" value="Enviar">
+        </div>
+    </form>
+        
+    <?php
+        include("send.php");
+    ?>
+</body>
+</html>
